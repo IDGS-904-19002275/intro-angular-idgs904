1b7c929b3dd3cc38a98979c869bf7f920f74458d
Soulz <19002275@alumnos.utleon.edu.mx>
2023-07-05 20:10:42 -0600

    JJML05/07/2023

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,24 +1,20 @@
-import { RouterModule, Routes } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { IricComponent } from "./escuela/iric/iric.component";
 import { OperasBasComponent } from "./escuela/formularios/operas-bas/operas-bas.component";
 import { AlumnoReactiveComponent } from "./formularios/alumno-reactive/alumno-reactive.component";
 
+
 const routes:Routes=[
     {path:'',redirectTo:'/home', pathMatch:'full'},
-    {path:'Iric', component : IricComponent},
-    {path:'Opersbas', component : OperasBasComponent},
-    {path:'Form', component : AlumnoReactiveComponent},
-    
-]
-
+    {path:'Iric',component:IricComponent},
+    {path:'Operas',component:OperasBasComponent},
+    {path:'FormReactive',component:AlumnoReactiveComponent}
+];
 
 @NgModule({
     imports:[RouterModule.forRoot(routes)],
     exports:[RouterModule]
-
-
-
 })
 
 export class AppRoutingModule{}
